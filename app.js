fa247afd851b89f0580f9e7b770d5cfe8c46dec3
sevynnewma935-max <sevynnewma935@gmail.com>
2026-09-04 23:21:27 +0300

    PWA%20v89

--- app.js
+++ app.js
@@ -4404,7 +4404,7 @@ mapMeasureHint: $('mapMeasureHint'),
     const url = URL.createObjectURL(blob);
     const link = document.createElement('a');
     link.href = url;
-    link.download = 'zone-clock-test-v88.csv';
+    link.download = 'zone-clock-test-v89.csv';
     document.body.appendChild(link);
     link.click();
     link.remove();
