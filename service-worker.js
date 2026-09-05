14e6d0b318399d6d8e6e244b2f24d45de6cb1bad
sevynnewma935-max <sevynnewma935@gmail.com>
2026-09-05 14:10:47 +0300

    PWA%20v105

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const APP_CACHE = 'stalker2-zone-clock-app-v102';
-const MAP_CACHE = 'stalker2-zone-clock-map-v102';
+const APP_CACHE = 'stalker2-zone-clock-app-v105';
+const MAP_CACHE = 'stalker2-zone-clock-map-v105';
 
 const APP_ASSETS = [
   './',
